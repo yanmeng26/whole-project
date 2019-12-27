d25cf61756e520009e30a0005060cd99763bceb8
yanmeng26 <yanme@oregonstate.edu>
2019-12-27 00:30:53 -0700

    Updates

---
+++ precache-manifest.fb1e0416918c3f1416b5a70c47cc8253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb006648f54c7b382578918ebe9d4bb",
+    "url": "/whole-projects/index.html"
+  },
+  {
+    "revision": "3fa7557753ffc9b7d49b",
+    "url": "/whole-projects/static/css/main.d1571c6e.chunk.css"
+  },
+  {
+    "revision": "dbdd6f4e62589ab68836",
+    "url": "/whole-projects/static/js/2.d8d60427.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/whole-projects/static/js/2.d8d60427.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fa7557753ffc9b7d49b",
+    "url": "/whole-projects/static/js/main.2940ae93.chunk.js"
+  },
+  {
+    "revision": "b94fe9b241580f0e412a",
+    "url": "/whole-projects/static/js/runtime-main.ded35127.js"
+  }
+]);
