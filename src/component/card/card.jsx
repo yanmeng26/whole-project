53aedd37f8b5c6795ed4366e5d6c00dca7ba61bb
yanmeng26 <yanme@oregonstate.edu>
2019-12-27 00:20:54 -0700

    fix can't load img bug

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -4,7 +4,7 @@ import "./card.css"
 export const Card = (props)=>(
 <div className = "card-container">
     
-    <img className = "card-img" alt = "Recruiter" src={`/img/${props.project.id}.png`} width={400} height={300}  mode='fit'/>
+    <img className = "card-img" alt = "Recruiter" src={process.env.PUBLIC_URL +`/img/${props.project.id}.png`} width={400} height={300}  mode='fit'/>
     <h2>{props.project.name}</h2>
     <p> <a href ={props.project.link}>{props.project.link}</a></p>
 </div>
